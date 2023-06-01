dbcd974240ea451ae64f52ab7a8f36084dc8c7c0
daveclinton <clintondavid46@gmail.com>
2023-06-01 15:39:02 +0300

    Feat: Add product model schema

---
+++ models/productModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  subCategories: [
+    {
+      name: { type: String, required: true },
+      products: [
+        {
+          brand: { type: String, required: true },
+          name: { type: String, required: true },
+          price: { type: Number, required: true },
+          shop: { type: String, required: true },
+          image: { type: String, required: String },
+        },
+      ],
+    },
+  ],
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
