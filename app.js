278e200ed11fbab603baa34b87ea7508ee6ab605
daveclinton <clintondavid46@gmail.com>
2023-06-01 14:54:25 +0300

    Feat: Add dotenv for mongo connection

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
+
 const PORT = 3000;
 
 const connectMongo = require("./config/database");
--- config/database.js
+++ config/database.js
@@ -3,16 +3,14 @@ mongoose.set("strictQuery", false);
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://kasio:pAssword12345@kasio.vfqulwp.mongodb.net/kasio?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log("Db is connected successfully");
   } catch (error) {
-    console.log(`Error, ${error.message}`);
+    console.log(`Error connecting to Database, ${error.message}`);
   }
 };
 
